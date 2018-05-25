69ad9d6ef12e0c41f7ff52eb48ebe2b7d74256f8
vamshikrishnaanandesi <vamshikrishnaanandesi@gmail.com>
2018-05-25 12:03:07 +0530

    Application Form

--- application_form/applicationController.js
+++ application_form/applicationController.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 var User = require('./application');
+var Submit = require('./application.js');
 
 // CREATES A NEW USER
 router.post('/', function (req, res) {
@@ -18,22 +19,45 @@ router.post('/', function (req, res) {
         });
 });
 
-// RETURNS ALL THE USERS IN THE DATABASE
-router.get('/', function (req, res) {
-    User.find({}, function (err, appl) {
-        if (err) return res.status(500).send("There was a problem finding the users.");
-        res.status(200).send(appl);
-    });
+// User answered question
+
+router.post('/user_answered', function (req, res) {
+    User.create({
+            question_user_answered : req.body.question_user_answered,
+        }, 
+        function (err, ques) {
+            if (err) return res.status(500).send("There was a problem adding the information to the database.");
+            res.status(200).send(ques);
+        });
 });
+
+//RETURNS ALL THE USERS IN THE DATABASE
+// router.get('/', function (req, res) {
+//     User.find({}, function (err, appl) {
+//         if (err) return res.status(500).send("There was a problem finding the users.");
+//         res.status(200).send(appl);
+//     });
+// });
 
 // GETS A SINGLE USER FROM THE DATABASE
 router.get('/:id', function (req, res) {
     User.findById(req.params.id, function (err, appls) {
         if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
+        if (!appls) return res.status(404).send("No user found.");
         res.status(200).send(appls);
     });
 });
 
+router.get('/', function (req, res) {
+    Submit
+    .find()
+    .select("_id question_name question_actual_answer question_user_answered")
+    .populate('user_details')
+    .exec(function (err, subm) {
+        if (err) return res.status(500).send("Tere was a problem finding the user.");
+        if (!subm) return res.status(404).send("No user found");
+        res.status(200).send(subm);
+    });
+});
 
 module.exports = router;
--- application_form/submitted_form.js
+++ application_form/submitted_form.js
@@ -1,13 +1,4 @@
 var mongoose1 = require('mongoose');
-var ApplicationForm = new mongoose1.Schema({
-    question_name: String,
-    question_actual_answer: String,
-    question_user_answered: String
-});
-
-mongoose1.model('application', ApplicationForm);
-
-module.exports = mongoose1.model('application');
 
 var submittedApplicationForm = new mongoose1.Schema({
     user_details: { type: mongoose1.Schema.Types.ObjectId, ref: 'application'}
